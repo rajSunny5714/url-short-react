2a784b7b8e14bb7da3f6704d6a7a4dc558365334
Sunny Raj <153062846+rajSunny5714@users.noreply.github.com>
2025-10-03 18:39:18 +0530

    Update AppRouter.jsx

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,48 +1,74 @@
-import { Route, Routes } from "react-router-dom";
-import NavBar from "./components/NavBar";
-import ShortenUrlPage from "./dashboard/ShortenUrlPage";
-import { Toaster } from "react-hot-toast";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Navbar from "./components/NavBar";
 import Footer from "./components/Footer";
 import LandingPage from "./components/LandingPage";
 import AboutPage from "./components/AboutPage";
 import RegisterPage from "./components/RegisterPage";
 import LoginPage from "./components/LoginPage";
 import DashboardLayout from "./dashboard/DashboardLayout";
+import ShortenUrlPage from "./dashboard/ShortenUrlPage";
 import PrivateRoute from "./PrivateRoute";
 import ErrorPage from "./components/ErrorPage";
-import Navbar from "./components/NavBar";
+import { Toaster } from "react-hot-toast";
 
 const AppRouter = () => {
+  const location = useLocation();
   const hideHeaderFooter = location.pathname.startsWith("/s");
 
-    return (
-        <>
-       {!hideHeaderFooter && <Navbar /> }
-       <Toaster position='bottom-center'/>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/s/:url" element={<ShortenUrlPage />} />
+  return (
+    <>
+      {!hideHeaderFooter && <Navbar />}
+      <Toaster position="bottom-center" />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/s/:url" element={<ShortenUrlPage />} />
 
-          <Route path="/register" element={<PrivateRoute publicPage={true}><RegisterPage /></PrivateRoute>} />
-          <Route path="/login" element={<PrivateRoute publicPage={true}><LoginPage /></PrivateRoute>} />
-          
-          <Route path="/dashboard" element={ <PrivateRoute publicPage={false}><DashboardLayout /></PrivateRoute>} />
-          <Route path="/error" element={ <ErrorPage />} />
-          <Route path="*" element={ <ErrorPage message="We can't seem to find the page you're looking for"/>} />
-        </Routes>
-        {!hideHeaderFooter && <Footer />}
-      </>
-    );
-}
+        <Route
+          path="/register"
+          element={
+            <PrivateRoute publicPage={true}>
+              <RegisterPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <PrivateRoute publicPage={true}>
+              <LoginPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute publicPage={false}>
+              <DashboardLayout />
+            </PrivateRoute>
+          }
+        />
 
+        {/* Catch-all route for unknown URLs */}
+        <Route
+          path="*"
+          element={
+            <ErrorPage message="We can't seem to find the page you're looking for" />
+          }
+        />
+      </Routes>
+      {!hideHeaderFooter && <Footer />}
+    </>
+  );
+};
 
 export default AppRouter;
 
+// Optional: Separate subdomain router if needed
 export const SubDomainRouter = () => {
-    return (
-        <Routes>
-          <Route path="/:url" element={<ShortenUrlPage />} />
-        </Routes>
-    )
-}
+  return (
+    <Routes>
+      <Route path="/s/:url" element={<ShortenUrlPage />} />
+    </Routes>
+  );
+};
