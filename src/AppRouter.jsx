214d0dfd51b8a502ca383baca637905dabfd6db4
Sunny Raj <153062846+rajSunny5714@users.noreply.github.com>
2025-10-03 16:58:31 +0530

    Update AppRouter.jsx

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -12,58 +12,36 @@ import PrivateRoute from "./PrivateRoute";
 import ErrorPage from "./components/ErrorPage";
 
 const AppRouter = () => {
-  return (
-    <>
-      <NavBar />
-      <Toaster position="bottom-center" />
-      <Routes>
-        {/* Main Pages */}
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/about" element={<AboutPage />} />
+//   const hideHeaderFooter = location.pathname.startsWith("/s");
 
-        {/* Short URL route */}
-        <Route path="/s/:url" element={<ShortenUrlPage />} />
+    return (
+        <>
+       <NavBar/>
+       <Toaster position='bottom-center'/>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/s/:url" element={<ShortenUrlPage />} />
 
-        {/* Auth Pages */}
-        <Route
-          path="/register"
-          element={
-            <PrivateRoute publicPage={true}>
-              <RegisterPage />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <PrivateRoute publicPage={true}>
-              <LoginPage />
-            </PrivateRoute>
-          }
-        />
+          <Route path="/register" element={<PrivateRoute publicPage={true}><RegisterPage /></PrivateRoute>} />
+          <Route path="/login" element={<PrivateRoute publicPage={true}><LoginPage /></PrivateRoute>} />
+          
+          <Route path="/dashboard" element={ <PrivateRoute publicPage={false}><DashboardLayout /></PrivateRoute>} />
+          <Route path="/error" element={ <ErrorPage />} />
+          <Route path="*" element={ <ErrorPage message="We can't seem to find the page you're looking for"/>} />
+        </Routes>
+        <Footer/>
+      </>
+    );
+}
 
-        {/* Dashboard (Protected) */}
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateRoute publicPage={false}>
-              <DashboardLayout />
-            </PrivateRoute>
-          }
-        />
-
-        {/* Error Routes */}
-        <Route path="/error" element={<ErrorPage />} />
-        <Route
-          path="*"
-          element={
-            <ErrorPage message="We can't seem to find the page you're looking for" />
-          }
-        />
-      </Routes>
-      <Footer />
-    </>
-  );
-};
 
 export default AppRouter;
+
+export const SubDomainRouter = () => {
+    return (
+        <Routes>
+          <Route path="/:url" element={<ShortenUrlPage />} />
+        </Routes>
+    )
+}
